29621ab6577d5a5224ad290f90a5869c89f358e3
Tadeusz <tadeusz.potoniec@parhelium.com>
2023-01-23 10:13:34 +0100

    Programs from the course separated into separate projects. Content translated into English.

--- C++L2P2/main.cpp
+++ C++L2P2/main.cpp
@@ -3,12 +3,25 @@
 //  C++L2P2
 //
 //  Created by Tadeusz Potoniec on 23/01/2023.
-//
+//  Based on https://www.youtube.com/watch?v=ErOzmh3BiXU&list=PLOYHgt8dIdoxx0Y5wzs7CFpmBzb40PaDo
 
 #include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+string login, pass;
+int main() {
+    cout<<"Welcome in our bank!"<<endl;
+    cout<<"Enter login:";
+    cin>>login;
+    cout<<"Enter password:";
+    cin>>pass;
+    
+    if((login=="admin")&&(pass=="szarlotka"))
+    {
+        cout<<"Login done"<<endl;
+    }
+    else
+    {
+        cout<<"Incorrect login details"<<endl;
+    }
     return 0;
 }
